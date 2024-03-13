aede326bb8a36728baec298d59d412859ee1cb0f
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2024-03-13 22:53:17 +0500

    Temporary ignore https

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,6 +2,9 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        https: false
+    },
     build: {
         target: 'esnext'
     },
