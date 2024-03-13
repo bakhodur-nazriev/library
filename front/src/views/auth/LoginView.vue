29bb03a193c4aa087ca0124c91092e23ad45c42f
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2024-03-13 23:08:21 +0500

    Comment httpsAgent

--- front/src/views/auth/LoginView.vue
+++ front/src/views/auth/LoginView.vue
@@ -26,7 +26,7 @@ const login = async (e) => {
     password: password.value
   });
 
-  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+  // const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
   const headers = {
     'Content-Type': 'application/json',
@@ -35,7 +35,7 @@ const login = async (e) => {
   };
 
   await axios
-      .post('/login', payload,{headers, httpsAgent})
+      .post('/login', payload,{headers})
       .then(res => {
         loading.value = false;
         if (res.status === 200) {
